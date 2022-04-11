db01f790481a4a7087f74c665b617f003d04074b
Michael Brich <mike@toreda.com>
2022-04-11 13:12:59 -0700

    Added missing CacheItemid export to package exports in index.ts.

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ export {Cache} from './cache';
 export {Cacheable} from './cacheable';
 export {CacheIdFn} from './cache/id/fn';
 export {CacheItem} from './cache/item';
+export {cacheItemId, CacheItemId} from './cache/item/id';
 export {CfgData} from './cfg/data';
-export {cacheItemId} from './cache/item/id';
